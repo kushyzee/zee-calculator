9b52af4c606dc7a79761f16ebc46a49eaa934a9c
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-27 09:27:04 +0100

    Populate the display when number button clicked

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const display = document.querySelector(".output p");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 
-let firstNum = 0;
-let secondNum = 0;
-let operator = null;
+let firstNum = null;
+let secondNum = null;
+let operatorSign = null;
 
 const add = (firstNum, secondNum) => {
   return firstNum + secondNum;
@@ -11,9 +13,11 @@ const add = (firstNum, secondNum) => {
 const subtract = (firstNum, secondNum) => {
   return firstNum - secondNum;
 };
+
 const multiply = (firstNum, secondNum) => {
   return firstNum * secondNum;
 };
+
 const divide = (firstNum, secondNum) => {
   if (secondNum === 0) {
     display.textContent = "Haha, nice try";
@@ -21,3 +25,13 @@ const divide = (firstNum, secondNum) => {
   }
   return firstNum / secondNum;
 };
+
+const populateDisplay = (e) => {
+  let num = e.target.textContent;
+  if (display.textContent.length === 15) {
+    return
+  }
+  display.textContent += num;
+};
+
+numbers.forEach((number) => number.addEventListener("click", populateDisplay));
