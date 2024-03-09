224f82f812effdf7cd4c8d6fa89115ad5319c152
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-03-09 14:43:46 +0100

    reset most functions

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const operators = document.querySelectorAll(".operator");
 const equals = document.getElementById("equal");
 const clearBtn = document.getElementById("ac");
 
-let firstNum = null;
-let secondNum = null;
-let operatorSign = null;
-let resultUpdated = false;
-
 const add = (firstNum, secondNum) => {
   return firstNum + secondNum;
 };
@@ -29,73 +24,3 @@ const divide = (firstNum, secondNum) => {
   return firstNum / secondNum;
 };
 
-const doCalculation = () => {
-  secondNum = Number(secondNum);
-  firstNum = Number(firstNum);
-
-  let result = 0;
-  switch (operatorSign) {
-    case "add":
-      result = add(firstNum, secondNum);
-      break;
-
-    case "minus":
-      result = subtract(firstNum, secondNum);
-      break;
-
-    case "division":
-      result = divide(firstNum, secondNum);
-      break;
-
-    case "multiply":
-      result = multiply(firstNum, secondNum);
-      break;
-  }
-  display.textContent = result;
-  resultUpdated = true;
-};
-
-const updateGlobalVariables = (e) => {
-  if (operatorSign && firstNum && secondNum) {
-    doCalculation()
-    operatorSign = e.target.id;
-    firstNum = display.textContent;
-    console.log(firstNum, secondNum, operatorSign);
-  }
-  operatorSign = e.target.id;
-  display.textContent = ''
-};
-
-const populateDisplay = (e) => {
-  let num = e.target.textContent;
-
-  if (display.textContent.length === 15) {
-    return;
-  }
-
-  if (resultUpdated) {
-    firstNum = null;
-    secondNum = null;
-    operatorSign = null;
-    display.textContent = "";
-    resultUpdated = false;
-  }
-
-  display.textContent += num;
-  if (operatorSign && firstNum) {
-    secondNum = display.textContent;
-    // operatorSign = null;
-  }
-
-  else if (!secondNum) {
-    firstNum = display.textContent;
-  }
-  console.log(firstNum, secondNum);
-};
-
-numbers.forEach((number) => number.addEventListener("click", populateDisplay));
-operators.forEach((operator) =>
-  operator.addEventListener("click", updateGlobalVariables)
-);
-
-equals.addEventListener("click", doCalculation);
